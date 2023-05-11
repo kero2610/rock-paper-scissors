711f1a1c1e8b119b1ee831b42c3cbef5b76e3b36
Aiman <aiman991026@gmail.com>
2023-05-11 22:52:04 +0800

    Make algorithm for Rock Paper Scissors in script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Rock Paper Scissors
+1. create a userInput variable to ask input from user whether user choose Rock, Paper or Scissors
+2. create a computerChoice variable to get generated output between Rock, Paper and Scissors
+3. if userInput === Rock && computerChoice === Rock - a tie
+4. if userInput === Rock && computerChoice === Paper - computer wins
+5. if userInput === Rock && computerChoice === Scissors - user wins
+6. if userInput === Paper && computerChoice === Rock - user wins
+7. if userInput === Paper && computerChoice === Paper - a tie
+8. if userInput === Paper && computerChoice === Scissors - computer wins
+9. if userInput === Scissors && computerChoice === Rock - computer wins
+10. if userInput === Scissors && computerChoice === Paper - user wins
+5. if userInput === Scissors && computerChoice === Scissors - a tie
